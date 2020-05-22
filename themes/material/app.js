4895194ad004df7a51d18583bae3a6f8f4dcdd1b
Tharindu Jayasanka <35922615+TharinduX@users.noreply.github.com>
2020-05-22 06:16:28 +0530

    minor changes

--- themes/material/app.js
+++ themes/material/app.js
@@ -806,6 +806,7 @@ function file_video(path) {
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/plyr/3.6.2/plyr.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.3/js/mdui.min.js"></script>
 `;
 
   $('#content').html(content);
